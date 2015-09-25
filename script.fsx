7580c2c30411f015a1f159e15b8beec99a858035
Evelina Gabasova <evelina@evelinag.com>
2015-09-25 13:04:55 -0500

    Strangeloop talk materials

---
+++ script.fsx
@@ -0,0 +1,75 @@
+﻿// Download Z3 binaries from
+// https://github.com/Z3Prover
+
+#I @"z3-4.4.0-x64-win/bin/"
+#r "Microsoft.Z3.dll"
+
+open Microsoft.Z3 
+open System
+
+let ctx = new Context()
+
+let note1 = ctx.MkInt(1)
+let note5 = ctx.MkInt(5)
+
+let m1 = ctx.MkIntConst("m1")
+let m5 = ctx.MkIntConst("m5")
+
+let mult1 = ctx.MkMul([|note1:>ArithExpr; m1:>_|])
+let mult5 = ctx.MkMul([|note5:>ArithExpr; m5:>_|])
+let sum15 = ctx.MkAdd([|mult1; mult5|])
+let totalNotes = ctx.MkAdd([|m1 :>ArithExpr; m5 :>_ |])
+
+let value = 42
+let nNotes = 10
+
+let solver = ctx.MkSolver()
+
+solver.Add( [| ctx.MkEq(ctx.MkInt(value), sum15);
+               ctx.MkEq(ctx.MkInt(nNotes), totalNotes) |])
+
+let result = solver.Check([||])
+match result with
+| Status.UNSATISFIABLE -> printfn "Unsatisfiable"
+| Status.SATISFIABLE -> 
+    printfn "Satisfiable"                
+    let model = solver.Model
+    printfn "m1 = %O, m5 = %O" (model.Evaluate(m1)) (model.Evaluate(m5))
+| Status.UNKNOWN -> printfn "unknown"
+| _ -> printfn "Other"
+
+solver.Reset()
+
+
+
+#load "z3_quotations.fs"
+open Z3_quotations
+
+let context = new Context()
+let expression = <@ fun a1 a2 -> a1 * 5 + a2 * 1 = 2042 @>
+
+let rec findNotes expression (k:int) =
+    let vars, sum, expr = funcToExpr context expression
+    let totalCoins = context.MkAdd(vars)
+
+    let solver = context.MkSolver()
+    solver.Add( [| context.MkEq(sum, expr);
+                   context.MkEq(totalCoins, context.MkInt(k)) |])
+    solver.Add( vars |> Array.map (fun v -> context.MkGe(v, context.MkInt(0))))
+    let result = solver.Check([||])
+    match result with
+    | Status.UNSATISFIABLE -> 
+        printfn "Unsatisfiable for k = %d" k
+        findNotes expression (k+1)
+    | Status.SATISFIABLE -> 
+        printfn "Satisfiable for k = %d" k                
+        let model = solver.Model
+        for v in vars do
+            printfn "%s = %O" (v.SExpr()) (model.Evaluate(v))
+        
+    | Status.UNKNOWN -> printfn "unknown"
+    | _ -> printfn "Other"
+
+#time
+findNotes expression 1
+
---
+++ z3_quotations.fs
@@ -0,0 +1,33 @@
+﻿module Z3_quotations
+open Microsoft.Z3 
+open System
+
+open Microsoft.FSharp.Quotations
+
+let rec bodyToExpr (ctx:Context) (vars:Map<string, ArithExpr>) = function
+    | Patterns.Var(v) -> vars.[v.Name]
+    | Patterns.Value(v, t) when t = typeof<int> -> 
+        ctx.MkInt(v :?> int) :> ArithExpr
+    | DerivedPatterns.SpecificCall <@ (*) @> (None, _, [l; r]) -> 
+        let le = bodyToExpr ctx vars l
+        let re = bodyToExpr ctx vars r
+        ctx.MkMul [| le; re |]
+    | DerivedPatterns.SpecificCall <@ (+) @> (None, _, [l; r]) -> 
+        let le = bodyToExpr ctx vars l
+        let re = bodyToExpr ctx vars r
+        ctx.MkAdd [| le; re |]
+    | e -> failwithf "Unexpected: %A" e
+
+let funcToExpr (ctx:Context) = function 
+    | DerivedPatterns.Lambdas
+        (vars, DerivedPatterns.SpecificCall <@ (=) @> (None, _, [l; Patterns.Value(v, ty)])) 
+        when ty = typeof<int> ->
+        let vars = 
+            List.concat vars 
+            |> List.map (fun v -> v.Name, ctx.MkIntConst(v.Name) :> ArithExpr)
+        List.map snd vars |> Array.ofList,
+        ctx.MkInt(v :?> int),
+        bodyToExpr ctx (Map.ofList vars) l
+    | _ -> failwith "Expected function"
+
+
